64b01245a63bc4ca130db1505efcdadd30428fff
annygutierrez16 <agutilop@everis.com>
2020-01-07 08:28:12 -0500

    Creation of first block
    - Creation of first block.
    - Creation of test files.

---
+++ block.js
@@ -0,0 +1,20 @@
+class Block {
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        // Hash will be about 32 characters normally
+        return `
+          Timestamp: ${this.timestamp}
+          Last Hash: ${this.lastHash.substring(0, 10)}
+          Hash: ${this.hash.substring(0, 10)}
+          Data: ${this.data}
+        `;
+    }
+}
+
+module.exports = Block;
