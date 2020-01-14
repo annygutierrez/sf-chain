436cc6da2f14207a6f154e4d05cf2d606330ccbf
annygutierrez16 <agutilop@everis.com>
2020-01-14 08:17:46 -0500

    Add new test for the chain length and organize files
    - Add new test to the blockchain class.
    - Organize files.

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,4 +1,4 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('.');
 const Block = require('./block');
 
 describe('Blockchain', () => {
@@ -34,6 +34,18 @@ describe('Blockchain', () => {
         bc2.addBlock('foo');
         bc2.chain[1].data = 'Not foo';
         expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('Replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('Does not replace the chain with one of less than or equal to length', () => {
+        bc.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).not.toEqual(bc2.chain);
     });
 
 });
